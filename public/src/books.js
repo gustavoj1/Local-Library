f07f3c36fe8d1b3bcfc9d07de9134a51547bcdea
gustavoj1 <100816383+gustavoj1@users.noreply.github.com>
2022-04-14 21:56:51 -0400

    Complete qualified assignment, ...

---
+++ public/src/books.js
@@ -0,0 +1,31 @@
+function findAuthorById(authors, id) {
+  let found = authors.find((author) => author.id === id);
+  return found;  
+}
+
+function findBookById(books, id) {
+  let found = books.find((book) => book.id === id);
+  return found;
+}
+
+function partitionBooksByBorrowedStatus(books) {
+  let array1 = books.filter((book) => book.borrows[0].returned === false);
+  let array2 = books.filter((book) => book.borrows[0].returned !== false);
+  return [array1, array2];
+}
+
+function getBorrowersForBook(book, accounts) {
+  let result = book.borrows.map(borrower => {
+    let result = accounts.find(account => borrower.id === account.id);
+    result.returned = borrower.returned
+    return result;
+  })
+  return result.filter((borrower, i) => result.findIndex(item => item.id === borrower.id) === i);
+}
+
+module.exports = {
+  findAuthorById,
+  findBookById,
+  partitionBooksByBorrowedStatus,
+  getBorrowersForBook,
+};
